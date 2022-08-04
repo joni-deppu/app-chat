d7a64c3ddf80288774e4901c70bb7e3f0a4e91de
joni-deppu <44shuxichuan@gmail.com>
2022-08-04 17:51:48 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ end
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to root_path
+      redirect_to room_messages_path(@room.id)
     else
       render :new
     end
